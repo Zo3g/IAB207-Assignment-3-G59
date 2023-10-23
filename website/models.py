a2a9db35770b8c401c1d19ccf61c4bdbfad60d15
Jimmy <jamesmarto777@gmail.com>
2023-10-23 15:09:00 +1000

    class event

--- website/models.py
+++ website/models.py
@@ -12,3 +12,26 @@ class User(db.Model, UserMixin):
 
     def __repr__(self):
         return f"Name: {self.name}"
+
+class Event(db.Model):
+    __tablename__ = 'events'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    venuename = db.Column(db.String(200))
+    image = db.Column(db.String(400))
+    organiser = db.Column(db.String(80))
+    numticket = db.Column(db.Integer(80))
+    ticketcost = db.Column(db.Integer(80))
+    eventdate = db.Column(db.Datetime(80))
+    eventstart = db.Column(db.Datetime(80))
+    description = db.Column(db.String(200))
+    
+    def __repr__(self):
+        return f"Name: {self.name}"
+
+
+
+
+
+
+
