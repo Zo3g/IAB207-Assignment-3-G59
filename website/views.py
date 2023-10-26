a6431177916a3cd040f446d27adea88a960279b4
Jimmy <jamesmarto777@gmail.com>
2023-10-26 12:11:38 +1000

    Views

--- website/views.py
+++ website/views.py
@@ -1,8 +1,24 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+from .models import Event
+from . import db
+
 
 bp = Blueprint('main', __name__)
 
 
 @bp.route('/')
 def index():
-    return render_template('index.html')
+    events = db.session.scalars(db.select(Event)).all()
+    return render_template('index.html', events=events)
+
+@bp.route('/search')
+def search():
+    if request.args['search'] and request.args['search'] != "":
+        print(request.args['search'])
+        query = "%" + request.args['search'] + "%"
+        Events = db.session.scalars(db.select(Event)).where(Event.description.like(query))
+        return render_template('index.html', events=Events)
+    else:
+        return redirect(url_for('main.index'))
+
+
