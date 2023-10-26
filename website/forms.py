a45c21382e1d9b238c7cfb252dccf067a40318ae
linhn1400 <linhn1400@gmail.com>
2023-10-26 12:38:19 +1000

    Added class BookingForm

--- website/forms.py
+++ website/forms.py
@@ -6,41 +6,64 @@ from flask_wtf.file import FileRequired, FileField, FileAllowed
 
 ALLOWED_FILE = {'PNG', 'JPG', 'JPEG', 'png', 'jpg', 'jpeg'}
 
-#Create new event
-class EventForm(FlaskForm):
-  name = StringField('Your Event Name', validators=[InputRequired()])
-  venue = StringField('Venue', 
-            validators=[InputRequired()])
-  image = FileField('Destination Image', validators=[
-    FileRequired(message='Image cannot be empty'),
-    FileAllowed(ALLOWED_FILE, message='Only supports PNG, JPG, png, jpg')])
-  organiser = SelectField('Select', choices=['Faculty of Business', 'Faculty of Creative Industries', 'Faculty of Engineering', 'Faculty of Information Technology', 'Faculty of Sciences', 'QUT Bookclub', 'CODE Network', 'Debating Society', 'QUT Cheer and Dance', 'QUT Cliffhangers'])
-  numticket = IntegerField('Number of available tickets', validators=[InputRequired()])
-  ticketcost = FloatField('Cost of a single ticket', validators=[InputRequired()])
-  eventdatetime = DateTimeField('Event date & start time', format='%Y-%m-%d %H:%M:%S', validators=[InputRequired()])
-  description = TextAreaField('Description', 
-            validators=[InputRequired()])
-  submit = SubmitField("Create")
+# Create new event
 
-#creates the login information
+
+class EventForm(FlaskForm):
+    name = StringField('Your Event Name', validators=[InputRequired()])
+    venue = StringField('Venue',
+                        validators=[InputRequired()])
+    image = FileField('Destination Image', validators=[
+        FileRequired(message='Image cannot be empty'),
+        FileAllowed(ALLOWED_FILE, message='Only supports PNG, JPG, png, jpg')])
+    organiser = SelectField('Select', choices=['Faculty of Business', 'Faculty of Creative Industries', 'Faculty of Engineering', 'Faculty of Information Technology',
+                            'Faculty of Sciences', 'QUT Bookclub', 'CODE Network', 'Debating Society', 'QUT Cheer and Dance', 'QUT Cliffhangers'])
+    numticket = IntegerField(
+        'Number of available tickets', validators=[InputRequired()])
+    ticketcost = FloatField('Cost of a single ticket',
+                            validators=[InputRequired()])
+    eventdatetime = DateTimeField(
+        'Event date & start time', format='%Y-%m-%d %H:%M:%S', validators=[InputRequired()])
+    description = TextAreaField('Description',
+                                validators=[InputRequired()])
+    submit = SubmitField("Create")
+
+# creates the login information
+
+
 class LoginForm(FlaskForm):
-    user_name=StringField("User Name", validators=[InputRequired('Enter user name')])
-    password=PasswordField("Password", validators=[InputRequired('Enter user password')])
+    user_name = StringField("User Name", validators=[
+                            InputRequired('Enter user name')])
+    password = PasswordField("Password", validators=[
+                             InputRequired('Enter user password')])
     submit = SubmitField("Login")
 
  # this is the registration form
+
+
 class RegisterForm(FlaskForm):
-    user_name=StringField("User Name", validators=[InputRequired()])
-    email_id = StringField("Email Address", validators=[Email("Please enter a valid email")])
-    #linking two fields - password should be equal to data entered in confirm
-    password=PasswordField("Password", validators=[InputRequired(),
-                  EqualTo('confirm', message="Passwords should match")])
+    user_name = StringField("User Name", validators=[InputRequired()])
+    email_id = StringField("Email Address", validators=[
+                           Email("Please enter a valid email")])
+    # linking two fields - password should be equal to data entered in confirm
+    password = PasswordField("Password", validators=[InputRequired(),
+                                                     EqualTo('confirm', message="Passwords should match")])
     confirm = PasswordField("Confirm Password")
 
-    #submit button
+    # submit button
     submit = SubmitField("Register")
 
-    #User comment
+    # User comment
+
+
 class CommentForm(FlaskForm):
-  text = TextAreaField('Comment', [InputRequired()])
-  submit = SubmitField('Create')
+    text = TextAreaField('Comment', [InputRequired()])
+    submit = SubmitField('Create')
+
+    # Booking form
+
+
+class BookingForm(FlaskForm):
+    tickets = IntegerField('Number of tickets purchased',
+                           validators=[InputRequired()])
+    submit = SubmitField("Tickets selected")
