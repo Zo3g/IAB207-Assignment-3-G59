3f1b14a095b1f2b60f47b3ca4507f879d756a87a
linhn1400 <linhn1400@gmail.com>
2023-11-01 15:48:46 +1000

    Testing booking database

--- website/booking.py
+++ website/booking.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from .models import Event, Booking
-from .forms import BookingForm
+# from .forms import BookingForm
 from . import db
 import os
 from werkzeug.utils import secure_filename
@@ -10,8 +10,9 @@ from flask_login import login_required, current_user
 bookingbp = Blueprint('booking', __name__, url_prefix='/booking')
 
 
-@bookingbp.route('/booking', methods=['GET', 'POST'])
+@bookingbp.route('/booking/<userID>', methods=['GET', 'POST'])
 @login_required
-def book():
-    print('Method type: ', request.method)
-    form = BookingForm()
+def get_booking(userID):
+    booking = db.session.scalar(db.select(Booking).where(Booking.id == id))
+    booking = Booking.query.filter_by(id == userID)
+    return ('booking.html', booking == booking)
